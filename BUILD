aa74073e6020415359116337be22a0f2a2578e3d
Lulu Zhang <lulu@buildbuddy.io>
2023-02-24 18:28:42 -0800

    update

--- BUILD
+++ BUILD
@@ -1,1 +1,32 @@
-/home/lulu/review_bot/BUILD
+load("@io_bazel_rules_go//go:def.bzl", "go_binary", "go_library")
+
+# Prefer generated BUILD files to be called BUILD over BUILD.bazel
+# gazelle:build_file_name BUILD,BUILD.bazel
+# gazelle:prefix github.com/luluz66/review_bot
+load("@bazel_gazelle//:def.bzl", "gazelle")
+
+gazelle(name = "gazelle")
+
+gazelle(
+    name = "gazelle-update-repos",
+    args = [
+        "-from_file=go.mod",
+        "-to_macro=deps.bzl%go_dependencies",
+        "-prune",
+    ],
+    command = "update-repos",
+)
+
+go_library(
+    name = "review_bot_lib",
+    srcs = ["main.go"],
+    importpath = "github.com/luluz66/review_bot",
+    visibility = ["//visibility:private"],
+    deps = ["//app"],
+)
+
+go_binary(
+    name = "review_bot",
+    embed = [":review_bot_lib"],
+    visibility = ["//visibility:public"],
+)
--- main.go
+++ main.go
@@ -1,1 +1,53 @@
-/home/lulu/review_bot/main.go
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/luluz66/review_bot/app"
+)
+
+var (
+	appID          = flag.Int64("github.app.id", -1, "GitHub app ID.")
+	privateKeyPath = flag.String("github.app.private_key_path", "", "A Path to GitHub app private key.")
+	webHookSecret  = flag.String("github.app.webhook_secret", "", "webhook secret")
+	bbAPIKey       = flag.String("bb.api.key", "", "bb API Key")
+	port           = flag.Int64("github.app.port", 3000, "port")
+)
+
+func main() {
+	flag.Parse()
+	if appID == nil || *appID == -1 {
+		log.Fatal("require --github.app.id")
+	}
+	if privateKeyPath == nil || *privateKeyPath == "" {
+		log.Fatal("require --github.app.private_key_path")
+	}
+	if webHookSecret == nil || *webHookSecret == "" {
+		log.Fatal("require --github.app.webhook_secret")
+	}
+	ghApp, err := app.NewGithubApp(*appID, *privateKeyPath, *webHookSecret, *bbAPIKey)
+
+	if err != nil {
+		log.Fatalf("failed to create github app: %s", err)
+	}
+
+	addr := fmt.Sprintf("0.0.0.0:%d", *port)
+	log.Printf("Listening on http://%s", addr)
+	mux := http.NewServeMux()
+	handle(mux, "/event_handler", ghApp.HandleWebhook)
+	http.ListenAndServe(addr, mux)
+}
+
+func handle(mux *http.ServeMux, pattern string, handleFunc http.HandlerFunc) {
+	mux.HandleFunc(pattern, func(w http.ResponseWriter, req *http.Request) {
+		log.Printf("%s %s", req.Method, req.URL)
+		handleFunc(w, req)
+	})
+	if !strings.HasSuffix(pattern, "/") {
+		handle(mux, pattern+"/", handleFunc)
+	}
+}
